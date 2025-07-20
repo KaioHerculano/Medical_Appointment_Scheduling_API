d743166a6a5cb6e58728891bf19a057933ae1948
Kaio <kaioherculano12@gmail.com>
2025-07-20 17:11:59 -0400

    chore(deploy): Configure WhiteNoise and expose port 80

--- app/settings.py
+++ app/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -93,6 +94,7 @@ USE_TZ = True
 # Arquivos estáticos
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Configuração do campo auto
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
